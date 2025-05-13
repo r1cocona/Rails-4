451bb960388f4e2a7e3273ffdd03e1b618df5210
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-13 11:46:18 +0900

    #10 users controoler test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+setup do
+    @user = users(:one)
+  end
+  test "indexを取得する" do
+    get users_url
+    assert_response :success
+  end
+  test "newを取得する" do
+    get new_user_url
+    assert_response :success
+  end
+  test "ユーザー作成できる" do
+    assert_difference('User.count') do
+      post users_url, params: { user: {name: "taro"}}
+    end
+
+    assert_redirected_to user_url(User.last)
+  end
+  test "ユーザーをshowできる" do
+    get user_url(@user)
+    assert_response :success
+  end
+  test "editを取得する" do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+  test "ユーザーをupdateできる" do
+    patch user_url(@user), params: { user: { name: "iii"}}
+    assert_redirected_to user_url(@user)
+  end
+  test "ユーザーをdestroyできる" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+
+    assert_redirected_to root_path
+  end
 end
