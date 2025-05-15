0ea02785519d1f2943e3646893b0667d3f852403
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-15 16:19:42 +0900

    #10 テスト名を修正する

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   # setup do
-  #   @user = users(:one)
+  #   @user = users(:sato)
   # end
   test "indexアクションへのアクセスが成功" do
     get users_url
@@ -20,22 +20,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to user_url(User.last)
   end
   test "ユーザー一覧をshowできる" do
-    @user = users(:one)
+    @user = users(:sato)
     get user_url(@user)
     assert_response :success
   end
   test "editアクションにアクセスし、ステータスコード200なことを確かめる" do
-    @user = users(:one)
+    @user = users(:sato)
     get edit_user_url(@user)
     assert_response :success
   end
   test "ユーザーをupdateできる" do
-    @user = users(:one)
+    @user = users(:sato)
     patch user_url(@user), params: { user: { name: "iii"}}
     assert_redirected_to user_url(@user)
   end
   test "ユーザーをdestroyすると、一人減る" do
-    @user = users(:one)
+    @user = users(:sato)
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
