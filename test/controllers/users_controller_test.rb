306694ca9310f3aba8e4783d13e4f153f8b1494a
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-15 18:44:43 +0900

    #10 url -> path

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,42 +4,43 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # setup do
   #   @user = users(:sato)
   # end
-  test "indexアクションへのアクセスが成功" do
-    get users_url
+  test "indexアクションへのアクセスが200番で成功" do
+    get users_path
     assert_response :success
   end
-  test "newアクションへのアクセスが成功" do
-    get new_user_url
+  test "newアクションへのアクセスが200番で成功" do
+    get new_user_path
     assert_response :success
   end
   test "ユーザーを作成すると一人増える" do
     assert_difference('User.count') do
-      post users_url, params: { user: {name: "taro"}}
+      post users_path, params: { user: {name: "taro"}}
     end
-
-    assert_redirected_to user_url(User.last)
+    assert_response :redirect
+    assert_redirected_to user_path(User.last)
   end
   test "ユーザー一覧をshowできる" do
     @user = users(:sato)
-    get user_url(@user)
+    get user_path(@user)
     assert_response :success
   end
   test "editアクションにアクセスし、ステータスコード200なことを確かめる" do
     @user = users(:sato)
-    get edit_user_url(@user)
+    get edit_user_path(@user)
     assert_response :success
   end
   test "ユーザーをupdateできる" do
     @user = users(:sato)
-    patch user_url(@user), params: { user: { name: "iii"}}
-    assert_redirected_to user_url(@user)
+    patch user_path(@user), params: { user: { name: "iii"}}
+    assert_redirected_to user_path(@user)
   end
   test "ユーザーをdestroyすると、一人減る" do
     @user = users(:sato)
     assert_difference('User.count', -1) do
-      delete user_url(@user)
+      delete user_path(@user)
     end
 
+    assert_response :see_other
     assert_redirected_to root_path
   end
 end
