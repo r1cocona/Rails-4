1b88827063a7a995e92ec511ad00aa767c584686
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-15 14:30:11 +0900

    #10 共通化しない

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
+  # setup do
+  #   @user = users(:one)
+  # end
   test "indexアクションへのアクセスが成功" do
     get users_url
     assert_response :success
@@ -20,18 +20,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to user_url(User.last)
   end
   test "ユーザー一覧をshowできる" do
+    @user = users(:one)
     get user_url(@user)
     assert_response :success
   end
   test "editアクションにアクセスし、ステータスコード200なことを確かめる" do
+    @user = users(:one)
     get edit_user_url(@user)
     assert_response :success
   end
   test "ユーザーをupdateできる" do
+    @user = users(:one)
     patch user_url(@user), params: { user: { name: "iii"}}
     assert_redirected_to user_url(@user)
   end
   test "ユーザーをdestroyすると、一人減る" do
+    @user = users(:one)
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
