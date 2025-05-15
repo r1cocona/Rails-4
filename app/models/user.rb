203d5674a4df0cf156e6f8c993dc2dd650d3eb6a
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-15 20:07:12 +0900

    #13 validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     validates :name, presence: true
+    validates :ruby, presence: true
+    validates :sex, presence: true, inclusion: { in: %w(男 女), message: "%{value} is not a valid sex" }
+    validates :tel, allow_blank: true, format: { with: /\A0[-\d]{9,12}\z/, message: "must be a valid phone number" }
+    validates :mobile, allow_blank: true, format: { with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "must be a valid mobile number" }
+    validates :mail, presence: true, uniqueness: true, format: { with: /\A[\w+-.@?]+/i, message: "must be a valid email address" }
+    validates :zip, presence: true, format: { with: /\A\d{3}-?\d{4}\z/, message: "must be a valid postal code" }  
+    validates :address1, presence: true, inclusion: { in: address1s.keys }
+    validates :birthday, presence: true
 end
