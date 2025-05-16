7b2682bc3760f58c3ff9f103edf21548b509fccf
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-16 12:47:26 +0900

    #10 テストの名前を見やすいものに変更

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,36 +4,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # setup do
   #   @user = users(:sato)
   # end
-  test "indexアクションへのアクセスが200番で成功" do
+  test "#index ユーザー一覧ページのアクセスに成功すること" do
     get users_path
     assert_response :success
   end
-  test "newアクションへのアクセスが200番で成功" do
+  test "#new 新規作成ページのアクセスに成功すること" do
     get new_user_path
     assert_response :success
   end
-  test "ユーザーを作成すると一人増える" do
+  test "#create ユーザーを作成すると一人増える" do
     assert_difference('User.count') do
       post users_path, params: { user: {name: "taro"}}
     end
     assert_redirected_to user_path(User.last)
   end
-  test "ユーザー一覧をshowできる" do
+  test "#show ユーザー詳細画面のアクセスに成功すること" do
     @user = users(:sato)
     get user_path(@user)
     assert_response :success
   end
-  test "editアクションにアクセスし、ステータスコード200なことを確かめる" do
+  test "#edit 情報修正ページのアクセスに成功すること" do
     @user = users(:sato)
     get edit_user_path(@user)
     assert_response :success
   end
-  test "ユーザーをupdateできる" do
+  test "#update ユーザー情報が修正できる" do
     @user = users(:sato)
     patch user_path(@user), params: { user: { name: "iii"}}
     assert_redirected_to user_path(@user)
   end
-  test "ユーザーをdestroyすると、一人減る" do
+  test "#destroy ユーザー情報が削除できる" do
     @user = users(:sato)
     assert_difference('User.count', -1) do
       delete user_path(@user)
