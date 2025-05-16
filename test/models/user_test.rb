190279cecf14c7bde067e9ffaafd6b761dd1ae8b
r1kokona <kokona.matsuda@rizapgroup.com>
2025-05-16 11:30:07 +0900

    #13 validation のテスト

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,44 +15,44 @@ class UserTest < ActiveSupport::TestCase
 
     assert @user.valid?
   end
-  test "存在する名前である" do
-    @user = users(:sato)
-    @user.name = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在するルビである" do
-    @user = users(:sato)
-    @user.ruby = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する性別である" do
-    @user = users(:sato)
-    @user.sex = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する固定電話の番号である" do
-    @user = users(:sato)
-    @user.tel = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する電話番号である" do
-    @user = users(:sato)
-    @user.mobile = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する郵便番号である" do
-    @user = users(:sato)
-    @user.zip = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する住所である" do
-    @user = users(:sato)
-    @user.address1 = "   "
-    assert_not_nil @user.valid?
-  end
-  test "存在する生年月日である" do
-    @user = users(:sato)
-    @user.birthday = "   "
-    assert_not_nil @user.valid?
-  end
+  # test "存在する名前である" do
+  #   @user = users(:sato)
+  #   @user.name = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在するルビである" do
+  #   @user = users(:sato)
+  #   @user.ruby = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する性別である" do
+  #   @user = users(:sato)
+  #   @user.sex = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する固定電話の番号である" do
+  #   @user = users(:sato)
+  #   @user.tel = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する電話番号である" do
+  #   @user = users(:sato)
+  #   @user.mobile = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する郵便番号である" do
+  #   @user = users(:sato)
+  #   @user.zip = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する住所である" do
+  #   @user = users(:sato)
+  #   @user.address1 = "   "
+  #   assert_not_nil @user.valid?
+  # end
+  # test "存在する生年月日である" do
+  #   @user = users(:sato)
+  #   @user.birthday = "   "
+  #   assert_not_nil @user.valid?
+  # end
 end
